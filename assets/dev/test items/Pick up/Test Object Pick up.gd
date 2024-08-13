39aa8e2b383edd76c9e118d48f2526405a7298e1
Skraftz <33819609+Skraftz@users.noreply.github.com>
2024-08-12 22:44:59 -0400

    Test Object
    You can pick it up

---
+++ assets/dev/test items/Pick up/Test Object Pick up.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@export var item_scene:PackedScene
+@export var item_mesh:Mesh
+func _ready():
+	if item_scene == null:
+		queue_free()
+	if item_mesh != null:
+		$MeshInstance3D.mesh = item_mesh
--- scenes/player/InventoryManager.gd
+++ scenes/player/InventoryManager.gd
@@ -1,26 +1,24 @@
 extends Node
 
 var Inventory = []
-var CurrentItemIndex: int = 0
+var CurrentItem: int = 0
 
 func add_item(scene):
-	if true:
-		# return error
+	var child = get_node("Slot " + str(CurrentItem)).get_child_count()
+	if child == 1 or child == null: 
+		print_rich("[rainbow][tornado]Item OwO[/rainbow][/tornado]")
+		#Inventory.append(scene)
 		pass
 	else:
-		Inventory.append(scene)
+		print_rich("No item")
+
+func slot_check():
+	var integer_number_34:int = 0 #Probably unnecessary but better safe than never 
+	while integer_number_34 < 7:
+		get_node("Slot " + str(CurrentItem))
+		pass
 
 func drop_item(index):
 	var item = Inventory.pop_at(index)
 	# spawn item at the player's current location idk i need ritalin
 	
-func use_current_item():
-	pass
-
-# todo:
-# 
-# make add_item() functional
-# 
-# - equip item
-# - consume item
-# - 
